b8567d128d87e84b0e7e9e5ade0c4bbd65d95a18
Hardh S Kava <Hardh.Kava@iiitb.ac.in>
2026-04-25 18:55:59 +0530

    datatypes fixed

--- headers/structs.h
+++ headers/structs.h
@@ -1,16 +1,14 @@
 #ifndef STRUCTS_H
 #define STRUCTS_H
 
-#include <stdio.h>
 #include <pthread.h>
-#include <sempahore.h>
 
 enum Role{ADMIN, USER};
 
 struct DNSRecord{
 	char domain[100]; //eg google.co.in
 	char ip[16]; //12 digits, 3 dots, 1 \0
-}
+};
 
 struct CacheEntry{
 	struct DNSRecord record;
